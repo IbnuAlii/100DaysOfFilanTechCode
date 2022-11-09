f7ccc6a42f74021227938190e4f61d9078a750f3
[200~https://github.com/IbnuAlii~ <ibnuali600@gmail.com>
2022-11-10 00:29:50 +0300

    "day2"

---
+++ DSA Bootcamp-Java/Day 2/first-idea-program/src/primitives.java
@@ -0,0 +1,11 @@
+public class primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.76f;
+        double largeDecimalNumbers = 76767676.287;
+        long largeInteger = 5124141514541561615L;
+        boolean check = false;
+        // Ctrl + /
+    }
+}
