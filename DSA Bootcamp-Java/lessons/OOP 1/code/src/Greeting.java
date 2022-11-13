7cdac97c0f82a35a17ee5204e8f8c4452bb7d7d8
Mohamed Ali Nor <114952970+IbnuAlii@users.noreply.github.com>
2022-11-13 23:18:34 +0300

    OOP 1

---
+++ DSA Bootcamp-Java/lessons/OOP 1/code/src/Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    public static void main(String[] args) {
+
+
+         class salaan {
+            public static void main(String[] args) {
+                System.out.println("Hello world");
+            }
+        }
+    }
+}
---
+++ DSA Bootcamp-Java/lessons/OOP 1/code/src/message.java
@@ -0,0 +1,14 @@
+public class message {
+    public static void main(String[] args) {
+
+         class Message {
+            public static void main(String[] args) {
+
+            }
+
+            public static void message() {
+                System.out.println("This course is awesome");
+            }
+        }
+    }
+}
